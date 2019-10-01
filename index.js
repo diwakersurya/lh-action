201c8d3305e49d89f7e1f60c8c288b3010c04500
Diwaker Singh <diwaker.singh@inmobi.com>
2019-10-01 14:17:36 +0530

    wip

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ try {
             await waitOn(wOpts);
             // once here, all resources are available
             const lhr = await launchChromeAndRunLighthouse('http://localhost:5000', opts);
-            core.debug(JSON.stringify(lhr))
-            core.debug(`Lighthouse scores: ${Object.values(lhr.categories).map(c => c.score).join(', ')}`);
-            // core.debug(await execa.command("curl http://localhost:5000"));
+            console.log(JSON.stringify(lhr))
+            console.log(`Lighthouse scores: ${Object.values(lhr.categories).map(c => c.score).join(', ')}`);
+            console.log(await execa.command("curl http://localhost:5000"));
             await killNodeServer();
         } catch (e) {
             console.error("FAILED!!!!----------------", e);
