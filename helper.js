4e99185ed871a65660edbd09e4b30ac4c3ccd346
Diwaker Singh <diwaker.singh@inmobi.com>
2019-10-02 23:19:13 +0530

    wip

--- helper.js
+++ helper.js
@@ -10,7 +10,7 @@ const waitOnOptions = {
 };
 
 const chromeLauncherOptions = {
-        chromeFlags: ['--show-paint-rects', '--headless', "--ignore-certificate-errors","'--disable-gpu'"],
+        chromeFlags: ['--show-paint-rects', '--headless', "--ignore-certificate-errors","--disable-gpu","--allow-insecure-localhost"],
         logLevel: 'verbose'
 };
 
