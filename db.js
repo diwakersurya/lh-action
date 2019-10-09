84d193b8c27aa942885b33b0a3596915616ac6b7
Diwaker Singh <diwaker.singh@inmobi.com>
2019-10-09 22:39:20 +0530

    wip

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const axios=require('axios');
 const url="https://thetechlead-db-bot.glitch.me/thetechlead/db/";
 async function save(db,key,value){
-    return axios.put(`${url}/${dbName}`, {
+    return axios.put(`${url}/${db}`, {
     key,value
   })
 }
 
 async function get(db,key){
-    return axios.get(`${url}/${dbName}`);
+    return axios.get(`${url}/${db}`);
 }
 
 async function create(db){
-    return axios.post(`${url}/${dbName}`)
+    return axios.post(`${url}/${db}`)
 }
 
 module.exports={save,get,create};
