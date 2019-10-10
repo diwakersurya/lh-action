96ab1b43ef8a795049850bbe23b821423ccf6c65
Diwaker Singh <diwaker.singh@inmobi.com>
2019-10-10 10:52:40 +0530

    wip

--- dbHandler.js
+++ dbHandler.js
@@ -4,5 +4,6 @@ const dbName='lighthouse.json';
 module.exports=async function(lhData){
     //create the db
     await db.create(dbName);
+    console.log('db created>>>>>>>>>>>\n\n\n',dbName)
     await db.save(dbName,(new Date()).toISOString(),lhData)
 }
